5a28e032f7e20d439eee5aef46e590979b0b5f35
Daniel Bauer <daniel.bauer@senckenberg.de>
2024-07-12 12:34:53 +0200

    reduce species list after download

--- ModGP MASTER.R
+++ ModGP MASTER.R
@@ -158,19 +158,20 @@ source(file.path(Dir.Scripts,"ModGP-Outputs.R"))
 ## GBIF Data --------------------------------------------------------------
 message("Retrieving GBIF data")
 # test if we can run modgp on species level with this small hack
-GBIF_MODE = "ModGP"
-if (!is.na(strtoi(Sys.getenv("GBIF_WITH_SPECIES")))) {
-    message("Setting mode to Capfitogen for GBIF")
-    GBIF_MODE = "Capfitogen"
-}
+
 ## species of interest
 Species_ls <- FUN.DownGBIF(
 	species = SPECIES, # which species to pull data for
 	Dir = Dir.Data.GBIF, # where to store the data output on disk
 	Force = FALSE, # do not overwrite already present data
-	Mode = GBIF_MODE, # query download for entire genus
+	Mode = "ModGP", # query download for entire genus
 	parallel = 1 # no speed gain here for parallelising on personal machine
 	)
+
+if (!is.na(strtoi(Sys.getenv("SPECIES_LEVEL")))) {
+    Species_ls$occs <- Species_ls[names(Species_ls$occs) == SPECIES]
+	message(paste0("Reduced GBIF species list to", length(Species_ls$occs)))
+}
 
 ## Environmental Data -----------------------------------------------------
 message("Retrieving environmental data")
